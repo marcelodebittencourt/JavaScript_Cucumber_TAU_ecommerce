9a9762c781485c7acd1e1a8200693e4d71fc2d92
marcelobitt <marcelo@marcelodebittencourt.com>
2021-08-22 21:08:44 -0300

    Add Scenario Outline

--- pages/Home.js
+++ pages/Home.js
@@ -9,6 +9,18 @@ class Home {
     get searchButton() {
       return $(".btn.btn-default.button-search");
     }
+
+    get womenMenuOption() {
+      return $('ul.menu-content >li >a[title="Women"]');
+    }
+  
+    get dressesMenuOption() {
+      return $('ul.menu-content >li >a[title="Dresses"]');
+    }
+  
+    get tshirtMenuOption() {
+      return $('ul.menu-content >li >a[title="T-shirts"]');
+    }
   
     /**
      * Searches for a keyword
@@ -20,6 +32,28 @@ class Home {
       this.searchField.setValue(keyword);
       this.searchButton.click();
     }
+  
+    /**
+   * Clicks a menu option
+   *
+   * @param {String} option - Name of the menu option to click
+   */
+     selectMenuOption(option) {
+      switch (option) {
+        case "Women":
+          this.womenMenuOption.click();
+          break;
+        case "Dresses":
+          this.dressesMenuOption.click();
+          break;
+        case "T-Shirts":
+          this.tshirtMenuOption.click();
+          break;
+        default:
+          console.log(`Invalid Option ${option}`);
+      }
+    }
   }
+ 
   
   module.exports = new Home();
--- steps/Catalog/then.js
+++ steps/Catalog/then.js
@@ -2,6 +2,7 @@ import {Then} from "@cucumber/cucumber"
 import checkNoResultError from "../../support/assertions/checkNoResultError";
 import checkProducts from "../../support/assertions/checkProducts";
 import checkProductsContain from "../../support/assertions/checkProductsContain";
+import checkTitle from "../../support/assertions/checkTitle";
 
 Then(/^(no )?products are listed$/, notListed => {
     if (notListed) {
@@ -17,4 +18,8 @@ Then(/^(no )?products are listed$/, notListed => {
   
   Then(/^search results show products related to "(.*)"$/, keyword => {
     checkProductsContain(keyword);
+  });
+
+  Then(/^the title of the page should be "(.*)"$/, title => {
+    checkTitle(title);
   });
