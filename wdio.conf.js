d270c3cc685290751ca6d0f1f14edecb4fd91368
marcelobitt <marcelo@marcelodebittencourt.com>
2021-08-22 21:38:59 -0300

    change to use env to tags

--- wdio.conf.js
+++ wdio.conf.js
@@ -165,7 +165,7 @@ exports.config = {
         // <boolean> fail if there are any undefined or pending steps
         strict: false,
         // <string> (expression) only execute the features or scenarios with tags matching the expression
-        tagExpression: '@int',
+        tagExpression: `${process.env.ENVIRONMENT}`,
         // <number> timeout for step definitions
         timeout: 60000,
         // <boolean> Enable this config to treat undefined definitions as warnings.
